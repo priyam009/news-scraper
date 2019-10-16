77620a651d934fa41e7751e4d4a8fb07535c6019
Priyam <priyam009@live.com>
2019-10-17 00:08:14 +1100

    Update to add html route

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(express.static("./public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+require("./routes/htmlroutes")(app);
 require("./routes/apiroutes")(app);
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
