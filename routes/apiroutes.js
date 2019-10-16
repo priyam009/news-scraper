b198138dcb7c6347882e9b24fa118619b46e2a85
Priyam <priyam009@live.com>
2019-10-17 00:00:58 +1100

    Update home page route to link to handlebar

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -5,13 +5,19 @@ var axios = require("axios");
 //Require models
 var db = require("../models/");
 
-
 module.exports = function(app) {
-
   //Render home page
   app.get("/", function(req, res) {
+    //Find all articles in db
+    db.Article.find({})
+      .then(function(dbArticle) {
+        // res.json(dbArticle);
+        res.render("index", { articles: dbArticle });
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
     //Render index page
-    res.render("index");
   });
 
   //Get all articles
