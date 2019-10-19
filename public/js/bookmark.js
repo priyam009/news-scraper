8099a9950b714be582a8d119c3c0e8c0c9f2fd95
Priyam <priyam009@live.com>
2019-10-19 17:04:41 +1100

    Add functionalities to add, delete and render notes on events

--- public/js/bookmark.js
+++ public/js/bookmark.js
@@ -1,8 +1,106 @@
 $(document).ready(function() {
+  //Remove article from saved list
   $(document).on("click", ".remove-article", removeArticle);
 
+  //Clear Bookmark articles
   $("#clear-articles-bookmark").on("click", clearBookmark);
 
+  //Show all notes
+  $(".show-notes").on("click", showNotes);
+
+  //Save new notes
+  $(".save-changes").on("click", addNotes);
+
+  //Delete added note
+  $(document).on("click", ".remove-note", deleteNote);
+
+  //Show notes
+  function showNotes() {
+    var articleId = $(this).data("id");
+    //Get notes linked to the Article
+    getNotes(articleId);
+  }
+
+  //Add new notes for an article
+  function addNotes() {
+    var articleId = $(this).data("id");
+    var noteText = $(".note-input").val();
+
+    //Check if note exists
+    if (noteText) {
+      $.ajax({
+        method: "POST",
+        url: "/saved/" + articleId,
+        data: {
+          noteText: noteText
+        }
+      }).then(function(data) {
+        //Clear input field
+        $('input[type="text"]').val("");
+        //Get notes linked to the Article
+        getNotes(articleId);
+      });
+    }
+  }
+
+  //Get all notes for the article
+  function getNotes(articleId) {
+    $.ajax({
+      method: "GET",
+      url: "/saved/" + articleId
+    }).then(function(data) {
+      //Update modal Notes
+      updateNotes(data, articleId);
+    });
+  }
+
+  //Update/Create modal notes
+  function updateNotes(data, articleId) {
+    //Clear Modal content
+    $(".modal-title").empty();
+    $(".modal-body").empty();
+
+    // console.log("notes data", data);
+    $(".modal-title").text("Note " + articleId);
+    $(".save-changes").attr("data-id", articleId);
+
+    //Check if notes exists
+    if (data.note.length === 0) {
+      $(".modal-body").append("<h5>No Notes Yet</h5>");
+    } else {
+      for (var i = 0; i < data.note.length; i++) {
+        var note = "<h5>" + data.note[i].noteText + "</h5>";
+        var removeBtn =
+          "<button type='button' class='btn btn-secondary remove-note' " +
+          "data-id=" +
+          data.note[i]._id +
+          ">Remove</button>";
+
+        var div = $(
+          "<div class='d-flex flex-row justify-content-between mb-3'>"
+        ).append(note, removeBtn);
+        $(".modal-body").append(div);
+      }
+    }
+    $("#article-notes").modal({ focus: true });
+  }
+
+  //Delete Note for an article
+  function deleteNote() {
+    var noteId = $(this).data("id");
+
+    $.ajax({
+      method: "GET",
+      url: "/note/delete/" + noteId
+    }).then(function(data) {
+      console.log("Note deleted");
+      var articleId = $(".save-changes").attr("data-id");
+      //Get all notes for the article
+      getNotes(articleId);
+    });
+  }
+
+  //Delete Bookmarks
   function clearBookmark() {
     $.ajax({
       method: "GET",
